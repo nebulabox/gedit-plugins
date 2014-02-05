536d831c2d9b5f7dbaa1fdbe70f08e90f9223338
Paolo Borelli <pborelli@gnome.org>
2014-02-05 10:48:30 +0100

    Missing import

--- plugins/multiedit/multiedit/appactivatable.py
+++ plugins/multiedit/multiedit/appactivatable.py
@@ -19,7 +19,7 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301, USA.
 
-from gi.repository import GObject, Gedit
+from gi.repository import GObject, Gio, Gedit
 
 
 class MultiEditAppActivatable(GObject.Object, Gedit.AppActivatable):
