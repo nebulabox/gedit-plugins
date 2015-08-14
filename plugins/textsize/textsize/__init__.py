53bb37704cb4d492ff7b89ae69fdc9b66d4f446a
Ignacio Casal Quinteiro <icq@gnome.org>
2015-08-14 09:31:49 +0200

    textsize: add requires

--- plugins/textsize/textsize/__init__.py
+++ plugins/textsize/textsize/__init__.py
@@ -22,6 +22,9 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301, USA.
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gedit', '3.0')
 from gi.repository import GObject, Gio, Gtk, Gdk, Gedit
 from .viewactivatable import TextSizeViewActivatable
 import gettext
